bbb6ae8473f91830631aef66cae96aad80c175b4
Dmitrii2019 <dmitry.ml@mail.ru>
2019-12-08 01:13:18 +0300

    les_2_task_3

---
+++ Lesson 2/les_2_task_3.py
@@ -0,0 +1,6 @@
+# 3. Сформировать из введенного числа обратное по порядку входящих в него цифр и вывести на экран.
+# Например, если введено число 3486, надо вывести 6843.
+# https://drive.google.com/file/d/1zqcKzg3qTopPn_x2dO7g1M-K9P530_Fp/view?usp=sharing
+
+number = input('Введите число: ')
+print(''.join(reversed(number)))
