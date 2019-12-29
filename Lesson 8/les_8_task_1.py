706d5fa76a02595503299c27b511fa278cd16e2f
Dmitrii2019 <dmitry.ml@mail.ru>
2019-12-29 19:51:09 +0300

    les_8_task_1

---
+++ Lesson 8/les_8_task_1.py
@@ -0,0 +1,23 @@
+# 1) Определение количества различных подстрок с использованием хеш-функции.
+# Пусть на вход функции дана строка. Требуется вернуть количество различных подстрок в этой строке.
+# Примечание: в сумму не включаем пустую строку и строку целиком.
+# Пример работы функции:
+import hashlib
+
+line = 'papa'
+hash_ = []
+
+
+def number_substrings(line):
+    count = 0
+    len_line = len(line)
+    for i in range(len_line):
+        for j in range(len_line + 1):
+            if line[i:j] != line and line[i:j] != '':
+                if hashlib.sha256(line[i:j].encode('utf-8')).hexdigest() not in hash_:
+                    hash_.append(hashlib.sha256(line[i:j].encode('utf-8')).hexdigest())
+                    count += 1
+    return count
+
+
+print(number_substrings(line))
